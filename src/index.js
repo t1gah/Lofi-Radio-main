e01cfc5ad0472f524c6d0c93fb9a2c6dd2fcf253
t1gah <dk.rejoj@gmail.com>
2024-02-03 12:51:44 +0100

    new

--- src/index.js
+++ src/index.js
@@ -7,6 +7,26 @@ client._loadNodeEvents()
 client._loadPlayerEvents()
 client._loadSlashCommands()
 client.connect()
+//==============================================================
+const colors = require('colors')
+setInterval(() => {
+    if(!client || !client.user) {
+    console.log("The Client Didn't Login Proccesing Kill 1".brightRed)
+        process.kill(1);
+    } else {
+        console.log("The Client Has Logged In Successfully".brightGreen)
+    }
+}, 10000); 
+
+const express = require('express');
+const app = express();
+app.get('/', (req, res) => {
+  res.send('I am online now!')
+});
+app.listen(4200, () => {
+  console.log('Server started.');
+});
+//==============================================================
 
 process.on('unhandledRejection', (reason, p) => {
     console.log(reason, p);
